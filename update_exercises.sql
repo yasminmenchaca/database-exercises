45163c6d85c1d60b3f074a8a29bf3fcc4e554d0d
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-19 17:54:45 -0600

    in progress

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,3 +1,34 @@
 USE codeup_test_db;
 
-SELECT * AS 'Info';
+# All albums in your table.
+select *
+from albums;
+
+# All albums released before 1980
+select *
+from albums
+where release_date < 1980;
+
+# All albums by Michael Jackson
+select *
+from albums
+where artist = 'Michael Jackson';
+
+
+
+
+# After each SELECT add an UPDATE statement to:
+
+# Make all the albums 10 times more popular (sales * 10)
+update albums
+set sales = sales * 10;
+
+# Make all the albums 10 times more popular (sales * 10)
+update albums
+set release_date = release_date - 100
+where release_date < 1980;
+
+# Change 'Michael Jackson' to 'Peter Jackson'
+update albums
+set artist = 'Peter Jackson'
+where artist = 'Michael Jackson';
