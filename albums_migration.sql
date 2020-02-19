5b899751e374ebedad7ef216d496c0c19cbc4c1a
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-19 13:34:24 -0600

    updated README.md

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,6 +2,7 @@ CREATE DATABASE IF NOT EXISTS codeup_test_db;
 
 USE codeup_test_db;
 
+-- creates 'albums' table if it doesn't exist;
 CREATE TABLE IF NOT EXISTS albums
 (
     id           INT UNSIGNED   NOT NULL AUTO_INCREMENT,
@@ -9,6 +10,6 @@ CREATE TABLE IF NOT EXISTS albums
     name         VARCHAR(500)   NOT NULL,
     release_date INT(4)         NOT NULL,
     sales        DECIMAL(10, 2) NOT NULL,
-    genre        VARCHAR(10)    NOT NULL,
+    genre        VARCHAR(20)    NOT NULL,
     PRIMARY KEY (id)
 );
