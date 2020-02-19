70c5ffd93c18bb61dfea18ac5abf9bbd3eebd4fd
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-19 13:53:55 -0600

    in progress

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,14 +2,15 @@ CREATE DATABASE IF NOT EXISTS codeup_test_db;
 
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
 -- creates 'albums' table if it doesn't exist;
-CREATE TABLE IF NOT EXISTS albums
+CREATE TABLE albums
 (
-    id           INT UNSIGNED   NOT NULL AUTO_INCREMENT,
-    artist       VARCHAR(50)    NOT NULL,
-    name         VARCHAR(500)   NOT NULL,
-    release_date INT(4)         NOT NULL,
-    sales        DECIMAL(10, 2) NOT NULL,
-    genre        VARCHAR(20)    NOT NULL,
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(100)  NOT NULL,
+    name         VARCHAR(100) NOT NULL,
+    release_date INT(4)       NOT NULL,
+    sales        FLOAT        NOT NULL,
+    genre        VARCHAR(50)  NOT NULL,
     PRIMARY KEY (id)
 );
