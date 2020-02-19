b60573267c2809fedcd789a6729f5a80c63e773b
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-19 15:26:33 -0600

    in progress

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,31 +1,31 @@
 USE codeup_test_db;
 
-SELECT 'All of Pink Floyd''s albums: ' AS 'Info';
+SELECT 'Pink Floyd' AS 'Here are the albums from:';
 SELECT name
 FROM albums
 WHERE artist = 'Pink Floyd';
 
-SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released: ' AS 'Info';
+SELECT 'The Beatles' AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released: ';
 SELECT release_date
 FROM albums
 WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-SELECT 'The genre for Nevermind: ' AS 'Info';
+SELECT 'Nirvana' AS 'The genre for Nevermind: ';
 SELECT genre
 FROM albums
 WHERE name = 'Nevermind';
 
-SELECT 'Albums released in the 1990s: ' AS 'Info';
+SELECT 'the 1990''s' AS 'Albums released in the 1990s: ';
 SELECT release_date
 FROM albums
 WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT 'Albums that had less than 20 million certified sales: ' AS 'Info';
+SELECT 'Less than 20 million' AS 'Albums that had less than 20 million certified sales: ';
 SELECT sales
 FROM albums
 WHERE sales < 20;
 
-SELECT 'All the albums with a genre of "Rock": ' AS 'Info';
+SELECT 'Rock' AS 'All the albums with a genre of "Rock": ';
 SELECT name
 FROM albums
 WHERE genre = 'Rock';
