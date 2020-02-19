66878c12fdac845ac7d8608e5ccf3f2e3dc232fd
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-19 15:11:57 -0600

    in progress

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,10 +6,10 @@ USE codeup_test_db;
 CREATE TABLE IF NOT EXISTS albums
 (
     id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist       VARCHAR(100)  NOT NULL,
+    artist       VARCHAR(100) NOT NULL,
     name         VARCHAR(100) NOT NULL,
     release_date INT(4)       NOT NULL,
     sales        FLOAT        NOT NULL,
-    genre        VARCHAR(100)  NOT NULL,
+    genre        VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,5 +1,31 @@
 USE codeup_test_db;
 
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT 'All of Pink Floyd''s albums: ' AS 'Info';
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released: ' AS 'Info';
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT 'The genre for Nevermind: ' AS 'Info';
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT 'Albums released in the 1990s: ' AS 'Info';
+SELECT release_date
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT 'Albums that had less than 20 million certified sales: ' AS 'Info';
+SELECT sales
+FROM albums
+WHERE sales < 20;
+
+SELECT 'All the albums with a genre of "Rock": ' AS 'Info';
+SELECT name
+FROM albums
+WHERE genre = 'Rock';
