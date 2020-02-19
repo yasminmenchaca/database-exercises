c416c4b858cc28ee8d96d49e5d746091b16e6834
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-19 11:26:22 -0600

    in progress

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+
+USE codeup_test_db;
+
+CREATE TABLE albums
+(
+    id           INT UNSIGNED   NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(50)    NOT NULL,
+    name         VARCHAR(500)   NOT NULL,
+    release_date INT(4)         NOT NULL,
+    sales        DECIMAL(10, 2) NOT NULL,
+    genre        VARCHAR(10)    NOT NULL,
+    PRIMARY KEY (id)
+);
