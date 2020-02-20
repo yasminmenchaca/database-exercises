a997fcd0d3e3c683dacb34dbadb0ce26c972bbe8
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-19 18:35:52 -0600

    completed

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,5 +1,7 @@
 USE codeup_test_db;
 
+TRUNCATE albums;
+
 INSERT INTO albums(artist, name, release_date, sales, genre)
 VALUES ('Michael Jackson', 'Thriller', 1982, 47.3, 'Pop, rock, R&B'),
        ('AC/DC', 'Back in Black', 1980, 26.4, 'Hard rock'),
