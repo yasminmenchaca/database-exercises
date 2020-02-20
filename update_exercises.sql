968ba10de46e0d35d9caf48d3e10e000459aaabe
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-19 18:16:22 -0600

    completed

--- update_exercises.sql
+++ update_exercises.sql
@@ -14,21 +14,29 @@ select *
 from albums
 where artist = 'Michael Jackson';
 
-
-
-
 # After each SELECT add an UPDATE statement to:
 
 # Make all the albums 10 times more popular (sales * 10)
 update albums
 set sales = sales * 10;
 
-# Make all the albums 10 times more popular (sales * 10)
+select *
+from albums;
+
+# Move all the albums before 1980 back to the 1800s.
 update albums
 set release_date = release_date - 100
 where release_date < 1980;
+
+select *
+from albums
+where release_date < 1880;
 
 # Change 'Michael Jackson' to 'Peter Jackson'
 update albums
 set artist = 'Peter Jackson'
 where artist = 'Michael Jackson';
+
+select *
+from albums
+where artist = 'Peter Jackson';
