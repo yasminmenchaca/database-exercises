4a9dcdc0ce0b943a0b3c399fcef0ceda19355ac1
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-21 10:52:08 -0600

    completed

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -21,13 +21,13 @@ FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name ASC, first_name ASC;
 
-# Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
 
-SELECT *
+# Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'Full Name'
 FROM employees
 WHERE last_name LIKE 'E%'
-   OR last_name LIKE '%E'
-ORDER BY emp_no ASC;
+  AND last_name LIKE '%E';
 
 # Now reverse the sort order for both queries.
 
@@ -37,10 +37,10 @@ WHERE last_name LIKE 'E%'
    OR last_name LIKE '%E'
 ORDER BY emp_no DESC;
 
-# Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
 
-SELECT *
+SELECT CONCAT(first_name, ' ', last_name, ' has worked for the company for ', datediff(curdate(), hire_date),
+              ' days.') AS 'Days in Company'
 FROM employees
 WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
-  AND (birth_date LIKE '%-12-25')
-ORDER BY birth_date ASC, hire_date DESC;
+  AND (birth_date LIKE '%-12-25');
