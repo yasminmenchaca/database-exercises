4ed9603f060778217e0dee7c5e61c328d6a07107
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-21 11:05:51 -0600

    completed

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,27 +1,4 @@
 USE employees;
-
-# Modify your first query to order by first name. The first result should be Irena Reutenauer and the last result should be Vidya Simmen.
-
-SELECT *
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name ASC;
-
-# Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
-
-SELECT *
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name ASC, last_name ASC;
-
-# Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
-
-SELECT *
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name ASC, first_name ASC;
-
-
 # Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
 
 SELECT CONCAT(first_name, ' ', last_name) AS 'Full Name'
@@ -29,18 +6,11 @@ FROM employees
 WHERE last_name LIKE 'E%'
   AND last_name LIKE '%E';
 
-# Now reverse the sort order for both queries.
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-   OR last_name LIKE '%E'
-ORDER BY emp_no DESC;
-
 # For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
 
 SELECT CONCAT(first_name, ' ', last_name, ' has worked for the company for ', datediff(curdate(), hire_date),
               ' days.') AS 'Days in Company'
 FROM employees
 WHERE (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
-  AND (birth_date LIKE '%-12-25');
+  AND (birth_date LIKE '%-12-25')
+order by 'Days in Company';
